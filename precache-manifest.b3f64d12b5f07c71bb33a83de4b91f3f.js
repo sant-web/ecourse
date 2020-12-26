b327c9ee3063827d4e7b9573a88a3279145b15ec
Ankhbayar Enkhtaivan <mefbayar@gmail.com>
2020-12-26 21:22:14 +0800

    Updates

---
+++ precache-manifest.b3f64d12b5f07c71bb33a83de4b91f3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53ed531a7d52c705708c731c2b0980e",
+    "url": "/ecourse/index.html"
+  },
+  {
+    "revision": "007c19c5517719d06a57",
+    "url": "/ecourse/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "d963f562228965688918",
+    "url": "/ecourse/static/css/main.c99821fe.chunk.css"
+  },
+  {
+    "revision": "007c19c5517719d06a57",
+    "url": "/ecourse/static/js/2.58cb78bf.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/ecourse/static/js/2.58cb78bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d963f562228965688918",
+    "url": "/ecourse/static/js/main.db2abe62.chunk.js"
+  },
+  {
+    "revision": "29e4cc98b35e6fd8a403",
+    "url": "/ecourse/static/js/runtime-main.25194053.js"
+  },
+  {
+    "revision": "69181621962c27ead4e7988e92b902a8",
+    "url": "/ecourse/static/media/App.69181621.less"
+  }
+]);
